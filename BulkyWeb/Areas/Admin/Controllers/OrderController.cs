0945f742494665e1c8d01beef5d96bcc003ba72e
Zhandos Sailaubekov <jandakazah@mail.ru>
2023-07-29 18:51:07 +0600

    Post Deployment Url Settings

--- BulkyWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -139,7 +139,7 @@ namespace BulkyWeb.Areas.Admin.Controllers
                 .GetAll(u => u.OrderHeaderId == OrderVM.OrderHeader.Id, includeProperties: "Product");
 
             //stripe logic
-            var domain = "https://localhost:7209/";
+            var domain = Request.Scheme + "://" +Request.Host.Value + "/";
             var options = new SessionCreateOptions
             {
                 SuccessUrl = domain + $"admin/order/PaymentConfirmation?orderHeaderId={OrderVM.OrderHeader.Id}",
--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -127,7 +127,7 @@ namespace BulkyWeb.Areas.Customer.Controllers
 			{
                 //it is a regular customer account and we need to capture payment
                 //stripe logic
-                var domain = "https://localhost:7209/";
+                var domain = Request.Scheme + "://" + Request.Host.Value + "/";
                 var options = new SessionCreateOptions
                 {
                     SuccessUrl = domain + $"customer/cart/OrderConfirmation?id={ShoppingCartVM.OrderHeader.Id}",
