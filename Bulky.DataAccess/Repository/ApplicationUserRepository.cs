ba6fea73552cc45ad909f93907475964ed4ed568
Zhandos Sailaubekov <jandakazah@mail.ru>
2023-08-08 18:18:44 +0600

    Removed ApplicationDbContext in UserController to UnitOfWork for better and cleaner code readability

--- Bulky.DataAccess/Repository/ApplicationUserRepository.cs
+++ Bulky.DataAccess/Repository/ApplicationUserRepository.cs
@@ -18,5 +18,9 @@ namespace Bulky.DataAccess.Repository
             _db = db;
         }
 
+        public void Update(ApplicationUser applicationUser)
+        {
+           _db.ApplicationUsers.Update(applicationUser);
+        }
     }
 }
